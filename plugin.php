5dba8bf00640a7298e550291501e8e52cc7d6a3a
Justin Frydman <justin.frydman@gmail.com>
2021-06-22 16:07:55 -0600

    clean up plugin.php, remove unneeded wordpress-stubs

--- plugin.php
+++ plugin.php
@@ -5,8 +5,10 @@ namespace Tribe\Storage\Plugin;
 use Tribe\Storage\Plugin\Statically\Providers\Statically_Provider;
 use Tribe\Storage\Plugin\Statically\Statically_Definition_Provider;
 
+$loader = Plugin_Loader::get_instance();
+
 // Load this plugin's container definitions
-Plugin_Loader::get_instance()->add_definition_provider( new Statically_Definition_Provider() );
+$loader->add_definition_provider( new Statically_Definition_Provider() );
 
 // Load this plugin's service providers
-Plugin_Loader::get_instance()->add_service_provider( Statically_Provider::class );
+$loader->add_service_provider( Statically_Provider::class );
